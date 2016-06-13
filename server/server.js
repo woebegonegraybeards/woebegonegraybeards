d982bef9e94c8abe945e8cbaf4b39cec84cfeb75
Brett Celestre <bcelestre@gmail.com>
2016-06-13 12:50:31 -0700

    (feat) Updates server.js path on line 14

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());                                       // parse appli
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));   // parse application/vnd.api+json as json
 app.use(bodyParser.urlencoded({ extended: true }));               // parse application/x-www-form-urlencoded
 app.use(methodOverride('X-HTTP-Method-Override'));    // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(express.static(__dirname + './client'));     // Set the static file location to /client
+app.use(express.static(__dirname + '../client'));     // Set the static file location to /client
 
 // Sets up a User Router
 var userRouter = express.Router();
