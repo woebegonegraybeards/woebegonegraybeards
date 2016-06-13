3940e11f1d85334e60c710f5af686cccf00e4cf7
Brett Celestre <bcelestre@gmail.com>
2016-06-13 14:21:39 -0700

    (feat) Working server.js route to index.html

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,13 +4,14 @@
 module.exports = function(app) {
 
   // Handles GET requests
-  app.get('/', function(req, res) {
-    //
-  });
+  // app.get('/', function(req, res) {
+  
+  // });
   
   // Front End Routes - handle all angular requests
   app.get('*', function(req, res) {
-    res.sendfile('../client/index.html'); // load our public/index.html file
+    console.log('routes.js called');
+    res.sendfile('./client/index.html'); // load our public/index.html file
   });
 
 };
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ var express        = require('express');
 var app            = express();
 var bodyParser     = require('body-parser');
 var morgan         = require('morgan');       // used for logging incoming request
+var methodOverride = require('method-override');
 
 var port = process.env.PORT || 5000;
 
@@ -12,23 +13,30 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());                                       // parse application/json 
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));   // parse application/vnd.api+json as json
 app.use(bodyParser.urlencoded({ extended: true }));               // parse application/x-www-form-urlencoded
-app.use(express.static(__dirname + '../client'));     // Set the static file location to /client
+app.use(methodOverride('X-HTTP-Method-Override'));  // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
+app.use(express.static(__dirname + './client'));     // Set the static file location to /client
 
-// Sets up a User Router
-var userRouter = express.Router();
 // Creates an instance of an express router
 var router = express.Router();
+// Sets up a User Router
+// var twitterRouter = express.Router();
+// var instagramRouter = express.Router();
+// var facebookRouter = express.Router();
 
 // Prefixes all routes with /api
 app.use('/api', router);
 // Use user router for all user request
-app.use('/api/users', userRouter);
-// Injects our routers into their respective route files
-// require('/routes/twitter.js')(userRouter);
-// require('./routes/instagram.js')(userRouter);
-// require('./routes/facebook.js')(userRouter);
+// app.use('/api/users', userRouter);
 // Configures our routes
 require('./routes/routes.js')(app);
+
+
+// Injects our routers into their respective route files
+// require('./routes/twitter.js')(twitterRouter);
+// require('./routes/instagram.js')(instagramRouter);
+// require('./routes/facebook.js')(facebookRouter);
+
+
 
 // Starts app
 app.listen(port);
