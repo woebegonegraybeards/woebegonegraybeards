384b0afae7b378caad83857c58535da624d5567d
Brett Celestre <bcelestre@gmail.com>
2016-06-13 16:14:59 -0700

    (feat) Updates all routes

--- server/routes/facebook.js
+++ server/routes/facebook.js
@@ -1,2 +1,14 @@
 
 // server/routes/facebook.js
+
+module.exports = function(app) {
+
+  // Handles GET requests
+  app.get('/api/facebook', function(req, res) {
+    console.log('api facebook');
+    res.send(200, 'api facebook response');
+  });
+
+};
+    
+    
--- server/routes/instagram.js
+++ server/routes/instagram.js
@@ -1,2 +1,14 @@
 
 // server/routes/instagram.js
+
+module.exports = function(app) {
+
+  // Handles GET requests
+  app.get('/api/instagram', function(req, res) {
+    console.log('api instagram');
+    res.send(200, 'api instagram response');
+  });
+
+};
+    
+    
