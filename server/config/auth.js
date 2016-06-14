df61dd890becd7e2e5488c45fa1c5b9ace24f967
Brett Celestre <bcelestre@gmail.com>
2016-06-14 14:00:40 -0700

    (feat) Updates .gitignore

---
+++ server/config/auth.js
@@ -0,0 +1,23 @@
+
+// server/config/auth.js
+
+var Config = require('/config.js');
+
+module.exports = {
+
+ // https://github.com/jaredhanson/passport-twitter
+ twitterAuth: function(url) {
+   // return url.match(rValidUrl);
+ }
+ 
+ // https://github.com/jaredhanson/passport-instagram
+ instagramAuth: function(url) {
+   // return url.match(rValidUrl);
+ }
+ 
+ // https://github.com/jaredhanson/passport-facebook
+ facebookAuth: function(url) {
+   // return url.match(rValidUrl);
+ }
+
+}; 
