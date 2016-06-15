9a09282e210a6169545ab0f6da03cb116005a860
Andrew Tsao <andrew.tsao8@gmail.com>
2016-06-15 15:27:22 -0700

    (feature) Adds testing for session persistence

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
+var clean = require('gulp-clean');
 var changed = require('gulp-changed');
 var postcss = require('gulp-postcss');
 var postcssImport = require('postcss-import');
@@ -12,11 +13,13 @@ var mixins = require('postcss-mixins');
 var neat = require('postcss-neat');
 var simpleVars = require('postcss-simple-vars');
 var reset = require('postcss-css-reset');
+var color = require('postcss-color-function');
 
 var processors = [
   postcssImport,
   simpleVars,
   reset,
+  color,
   autoprefixer({ browsers: ["last 2 version", "safari 5", "ie > 9", "opera 12.1", "ios 6", "android 2.3"] }),
   precss,
   neat,
@@ -26,13 +29,17 @@ var processors = [
 
 gulp.task('css', function () {
   return gulp.src('./client/assets/css/src/style.css')
-  .pipe(changed('./client/assets/css'))
   .pipe(postcss(processors))
   .pipe(gulp.dest('./client/assets/css'));
 });
 
+gulp.task('clean-css', function() {
+  return gulp.src('./client/assets/css/style.css', {read: false})
+    .pipe(clean());
+});
+
 gulp.task('watch', function() {
-  gulp.watch('./client/assets/css/src/*.css', ['css']);
+  gulp.watch('./client/assets/css/src/*.css', ['clean-css', 'css']);
   });
 
 gulp.task('default', ['watch']);
---
+++ client/js/services/Login.js
@@ -0,0 +1,17 @@
+angular.module('ff.services', [])
+
+.service('Login', function($q, $http) {
+  var session = function() {
+    return $http({
+      method: 'GET',
+      url: '/session'
+    }).then(function(data) {
+      return data;
+    }, function(error) {
+      return error;
+    });
+  }
+  return {
+    session: session
+  };
+});
