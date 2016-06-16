98b9033d7b4eb86b620ad18eba2ca48d0aa8b928
Andrew Tsao <andrew.tsao8@gmail.com>
2016-06-15 17:57:47 -0700

    (feature) Adds Instagram calls

--- client/js/controllers/FeedController.js
+++ client/js/controllers/FeedController.js
@@ -1,12 +1,21 @@
 angular.module('ff.controllers').controller('FeedController', function($scope, Twitter, Instagram) {
   $scope.getTwitterData = function() {
     Twitter.getData().then(function(results) {
-      $scope.data = results.data;
+      $scope.twitterData = results.data;
+    }, function(error) {
+      console.error(error);
+    })
+  };
+
+  $scope.getInstagramData = function() {
+    Instagram.getData().then(function(results) {
+      $scope.instagramData = results.data;
     }, function(error) {
       console.error(error);
     })
   };
 
   $scope.getTwitterData(); // Initial call
+  $scope.getInstagramData(); // Initial call
 
 });
