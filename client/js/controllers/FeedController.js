0291c8d40451c74cf0ac187fd12a27870e27bd8d
Andrew Tsao <andrew.tsao8@gmail.com>
2016-06-22 19:27:29 -0700

    (feature) Cleans up FeedController

--- client/js/controllers/FeedController.js
+++ client/js/controllers/FeedController.js
@@ -1,73 +1,118 @@
 angular.module('ff.controllers').controller('FeedController', function($scope, Feed, Twitter, Instagram, $timeout, $q, $state) {
   
-  $scope.loading = true;
+  $scope.loading = true; // Flag for showing/hiding loading spinner GIF
 
   $scope.$watch(function() {
-    return Feed.getQuery();
+    return Feed.getQuery(); // Set watch expression; setQuery() done in MainController
   }, function(newQuery, oldQuery) {
+    // newQuery - current listener call
+    // oldQuery - previous listener call
+
     if (newQuery !== null) {                     // Makes sure there is a search term
-      $scope.query = newQuery;                   // Grabs query
-      Twitter.getData($scope.query)              // Call Twitter API with query
-        .then(function(results) {
-          if ( !results.data ) {                 // Checks to see if Twitter was authorized
-            $scope.twitterData = undefined;      // If not, set twitter data to undefined
-          } else {                               // If it was authorized, store the results data
-            $scope.twitterData = results.data;   // Stores the Twitter results array
-          }
-        }, function(error) {
-          console.error(error);
-        })
-        .then(function(){
-          Instagram.getData($scope.query).then(function(results) {  // Call Instagram API with query
-            // console.log('instagram results: ', results);
-            $scope.instagramData = results.data.data;               // Stores the Instagram results array
-          })                                                        // If we're not authorized, this will be set to undefined
-          .then(function(){
-            if ( $scope.twitterData !== undefined && $scope.instagramData !== undefined ){        // If both data sets exist
-              if ( $scope.twitterData.length <= 0 && $scope.instagramData.length <= 0 ){    // Check to make sure there are results
-                console.log('no results for either');
-                // redirect to home
-                // $window.location.href = '/';
-                $state.go('home');
-              } else {                                                  // If there are results
-                $scope.sort($scope.twitterData, $scope.instagramData);  // Invoke sort function with twitterData and instagramData
-              }
-            } else if ( $scope.twitterData !== undefined && $scope.instagramData === undefined ) {  // If only Twitter data exists
-              if ( $scope.twitterData.legnth <= 0 ){                  // Check to make sure there are results
-                console.log('no results for twitter');
-                // redirect to home
-                // $window.location.href = '/';
-                $state.go('home');
-              } else {                                                // If there are results
-                $scope.sort($scope.twitterData, null);                // Invoke only Twitter, pass null for Instagram
-              }
-            } else if ( $scope.instagramData !== undefined && $scope.twitterData === undefined ) {  // If only Instagram data exists
-              if ( $scope.instagramData.length <= 0 ){                // Check to make sure there are results
-                // redirect to home
-                // $window.location.href = '/';
-                $state.go('home');
-                console.log('no data:');
-                Feed.setDataExists(false);
-              } else {                                                // If there are results
-                $scope.sort(null, $scope.instagramData);              // Invoke only Instagram, pass null for Twitter
-              }
+      $scope.query = newQuery; // Grab search query
+
+      Twitter.getData($scope.query).then(function(results) {
+        // If Twitter was authorized, store the returned results array
+        // If not, set it to undefined
+
+        if (!results.data) {
+          $scope.twitterData = undefined;
+        } else {
+          $scope.twitterData = results.data;
+        }
+      }).then(function() {
+        Instagram.getData($scope.query).then(function(results) {
+          $scope.instagramData = results.data.data; // Store the returned results.data array
+        }).then(function(){
+          if ($scope.twitterData !== undefined && $scope.instagramData !== undefined) {
+            // Both Twitter and Instagram accounts were authorized
+
+            // Check whether data for both actually exists
+            if ($scope.twitterData.length <= 0 && $scope.instagramData.length <= 0) {
+              Feed.setDataExists(false); // Set flag for no data found alert
+              $state.go('home'); // Return state to home
+            } else {
+              $scope.sort($scope.twitterData, $scope.instagramData); // Invoke sort function
             }
-          })
-          .catch(function( err ) {
-            console.error(err);
-          });
-        })
-        .catch(function( err ) {
+          } else if ($scope.twitterData !== undefined && $scope.instagramData === undefined) {
+            // Only Twitter authorized
+
+            // Check whether Twitter data actually exists
+            if ($scope.twitterData.length <= 0){
+              Feed.setDataExists(false); // Set flag for no data found alert
+              $state.go('home'); // Return state to home
+            } else {
+              $scope.sort($scope.twitterData, null); // Invoke sort function
+            }
+          } else if ($scope.instagramData !== undefined && $scope.twitterData === undefined) {
+            // Only Instagram authorized
+
+            // Check whether Instagram data actually exists
+            if ($scope.instagramData.length <= 0){
+              Feed.setDataExists(false); // Set flag for no data found alert
+              $state.go('home'); // Return state to home
+            } else {
+              $scope.sort(null, $scope.instagramData); // Invoke sort function
+            }
+          }
+        }).catch(function(err) {
+          // Instagram catch()
           console.error(err);
         });
+      }).catch(function(err) {
+        // Twitter catch()
+        console.error(err);
+      });
     }
   }, true);
   
   // Sort function organizes the API responses from Twitter and Instagram into one final sorted array
-  $scope.sort = function( twitter, instagram ) {
-    $scope.unsorted = [];                 // Creates unsorted array
+  $scope.sort = function(twitter, instagram) {
+    // twitter = twitterData array
+    // instagram = instagramData array
+
+    $scope.unsorted = []; // Initialize unsorted array
     
-    var epochConverter = function(str){   // Designed to convert Twitters original user.created_at string into epoch time
+    if (instagram !== null) {
+      // Convert Instagram timestamp to be consistent with Twitter as epoch time
+
+      instagram.forEach(function(val){
+        var time = val.created_time;
+
+        val.created_at = parseInt(time) * 1000; // Convert Instagram created_at time to milliseconds
+        val.source_network = 'instagram'; // Add flag for data source
+        
+        $scope.unsorted.push(val); // Push each post into the unsorted array
+      });
+    };
+
+    if (twitter !== null) {              // Checks to see if Twitter data was passed
+      // Convert Twitter timestamp to be consistent with Instagram as epoch time
+      
+      twitter.forEach(function(val){
+        var time = val.created_at;
+        // var offset = val.user.utc_offset;
+        var epochTime = epochConverter(time); // Converts created_at string to an epoch time integer
+        var correctedTime = epochTime;
+
+        val.created_at = parseInt(correctedTime); // Turns epoch time to a string again after last line
+        val.source_network = 'twitter'; // Adds flag for data source
+        
+        $scope.unsorted.push(val); // Push each tweet into the unsorted array
+      });
+    };
+    
+    $scope.reverseSort = _.sortBy($scope.unsorted, function(val){
+      return val.created_at;
+    });
+    
+    $scope.sorted = $scope.reverseSort.reverse(); // Reverses sorted array so newest posts are on top
+
+    $scope.callWidgets(); // Call for Twitter and Instagram widgets to activate embedded post styling
+
+    function epochConverter(str){
+      // Convert Twitter's original created_at timestamp string into epoch time
+
       var fullDate = str,
           day = str.substring(8,10),
           mon = str.substring(4,7),
@@ -75,78 +120,33 @@ angular.module('ff.controllers').controller('FeedController', function($scope, F
           hour = str.substring(11,13),
           min = str.substring(14,16),
           sec = str.substring(17,19);
+      
       var mnths = { 
         Jan: 0, Feb: 1, Mar: 2, Apr: 3, May: 4, Jun: 5,
-        Jul: 6, Aug: 7, Sep: 8, Oct: 9, Nov: 10, Dec: 11 };
+        Jul: 6, Aug: 7, Sep: 8, Oct: 9, Nov: 10, Dec: 11
+      };
+      
       return Date.UTC(year, mnths[mon], day, hour, min, sec);
     };
-    
-    if ( instagram !== null) {            // Checks to see if Instagram data was passed
-      instagram.forEach(function(val){    // Converts Instagram created_time to uniform dat with Twitter
-        var time = val.created_time;
-        val.source_network = 'instagram';
-        val.created_at = parseInt(time) * 1000;
-        val.userName = val.user.full_name;
-      });
-      
-      instagram.forEach(function(val){  // If true, push data into unsorted array
-        $scope.unsorted.push(val);      // Pushes each post into the unsorted array
-      });
-    }
-
-    if ( twitter !== null) {              // Checks to see if Twitter data was passed
-      twitter.forEach(function(val){      // Converts Twitter created_at to epoch time to match Instagram
-        var time = val.created_at;
-        val.time_string = time;
-        var offset = val.user.utc_offset;
-        var epochTime = epochConverter(time);                   // Converts created_at string to an epoch time integer
-        var correctedTime = epochTime;
-        // var time2 = correctedTime.toString().substring(0,11);  // Makes the epoch time 10 digits like Instagram
-        val.source_network = 'twitter';                         // Adds a source_network property
-        val.created_at = parseInt(correctedTime);               // Turns epoch time to a string again after last line
-        val.userName = val.user.name;
-      });
-      
-      twitter.forEach(function(val){    // If true, push data into unsorted array
-        $scope.unsorted.push(val);      // Pushes each tweet into the unsorted array
-      });
-    }
-    
-    $scope.reverseSort = _.sortBy($scope.unsorted, function(val){    // Creates a sorted array out of unsorted array
-      return val.created_at;                                    // This new array is sorted by created_at property
-    });
-    
-    $scope.sorted = $scope.reverseSort.reverse();   // Reverses sorted array so newest posts are on top
-
-    $scope.callWidgets();
-    console.log('after sort: ', $scope.sorted);
   };
 
   $scope.refreshWidgets = function() {
-    twttr.widgets.load();
-    instgrm.Embeds.process();
-    $scope.loading = false;
+    twttr.widgets.load(); // Call Twitter widget function (external script that gets included)
+    instgrm.Embeds.process(); // Call Instagram widget function (external script that gets included)
+    $scope.loading = false; // Stop spinner
   };
 
   $scope.callWidgets = function() {
     var twitterWidget = Feed.getTwitterWidget();
     var instagramWidget = Feed.getInstagramWidget();
 
+    // Resolve widget calls before refreshing widgets and activating embedded post styling
+    // Timeout is to allow time for photos (if any) to download
     $q.all([twitterWidget, instagramWidget]).then(function(results) {
       $timeout(function() {
         $scope.refreshWidgets();
       }, 2500);
     });
   };
-
-
-  // $timeout(function() {
-  //   console.log('calling widget: ');
-  //   $.ajax({ url: 'http://platform.twitter.com/widgets.js', dataType: 'script', cache:true});
-    
-  //   $.ajax({ url: 'http://platform.instagram.com/en_US/embeds.js', dataType: 'script', cache:true});
-  //   $scope.refreshWidgets();
-  //   $scope.loading = false;
-  // }, 2500);
   
 });
