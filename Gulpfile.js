6a289c433e7e405c18271e9f5dd77fec4a2444bf
Andrew Tsao <andrew.tsao8@gmail.com>
2016-06-22 18:32:42 -0700

    (feature) Cleans up front end files and package files

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,17 +5,13 @@ var nodemon = require('gulp-nodemon');
 var clean = require('gulp-clean');
 var postcss = require('gulp-postcss');
 var postcssImport = require('postcss-import');
-// var precss = require('precss');
 var autoprefixer = require('autoprefixer');
 var opacity = require('postcss-opacity');
-// var mixins = require('postcss-mixins');
-// var neat = require('postcss-neat');
 var simpleVars = require('postcss-simple-vars');
 var reset = require('postcss-css-reset');
 var color = require('postcss-color-function');
 var simpleMediaQueries = require('postcss-simple-media-queries');
 var nested = require('postcss-nested');
-// var nested = require('postcss-nesting');
 var map = require('postcss-map');
 var calc = require('postcss-calc');
 var clearfix = require('postcss-clearfix');
@@ -33,10 +29,7 @@ var processors = [
   opacity,
   color,
   calc,
-  // precss,
   autoprefixer({ browsers: ["last 2 version", "safari 5", "ie > 9", "opera 12.1", "ios 6", "android 2.3"] }),
-  // neat,
-  // mixins,
   grid(settings.grid),
   reset
 ];
