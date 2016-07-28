5bf7dde644381ea0bd83619e3a35ce11944f811a
Brett Celestre <bcelestre@gmail.com>
2016-07-28 12:21:35 -0700

    (feat) Updates server

--- server/server.js
+++ server/server.js
@@ -17,10 +17,10 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());                                       // parse application/json 
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));   // parse application/vnd.api+json as json
 app.use(bodyParser.urlencoded({ extended: true }));               // parse application/x-www-form-urlencoded
-// app.use(express.static('client'));                                // Set the static file location to /client
+app.use(express.static('client'));                                // Set the static file location to /client
 app.use(cookieParser());
 app.use(methodOverride('X-HTTP-Method-Override'));  // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(express.static(__dirname + './client'));     // Set the static file location to /client
+// app.use(express.static(__dirname + './client'));     // Set the static file location to /client
 app.use(expressSession({ secret: 'meow', resave: true, saveUninitialized: true })); // Sets up our express session resave value
 
 // Creates an instance of an express router
